c9a4413e4a3e385e9a1333f32e1f4ba37234dd09
MKursatUzuner <66836581+MKursatUzuner@users.noreply.github.com>
2022-01-20 22:39:23 +0300

    Update MultizoneMeshCombiner.py

--- MultizoneMeshCombiner.py
+++ MultizoneMeshCombiner.py
@@ -9,14 +9,13 @@ import os
 
 print('Please enter the zone number\n')
 zone_num = int(input());
-zones = str(np.zeros(zone_num))
 print('Please enter the name for the combined mesh file with its extension.\nIf it does not exist, it will be created\n')
 totalName = input()
 total = open("%s"%totalName,"w+")
 total.write("NZONE= %d\n"%zone_num)
 for i in range(zone_num):
-    print('\nPlease enter the name of the Zone %d mesh file with its extension;\n'%(i+1))
     current_zone = 1 + i
+    print('\nPlease enter the name of the Zone %d mesh file with its extension;\n'%current_zone)
     zoneTempName = input()
     if os.path.isfile(zoneTempName):
         zoneTemp = open("%s"%zoneTempName,"r")
